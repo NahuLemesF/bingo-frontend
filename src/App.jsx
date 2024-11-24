6f2d145d5529da4c73eb457b46fa9c2754ef31ff
Nahuel <nahulem@gmail.com>
2024-11-24 00:10:56 -0300

    Primer commit
    -Creacion del Proyecto con Vite + React

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Bienvenido al Bingo Virtual</h1>
+      <button>Iniciar Juego</button>
+    </div>
+  )
+}
+
+export default App
